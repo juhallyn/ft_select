f95011e7a2770f494954a2eae1871ccec9468131
Julien HALLYNCK <juhallyn@student.42.fr>
2017-11-07 16:58:27 +0100

    refact code for pages and rm move file

--- srcs/move_row.c
+++ srcs/move_row.c
@@ -6,7 +6,7 @@
 /*   By: juhallyn <juhallyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/18 17:38:13 by juhallyn          #+#    #+#             */
-/*   Updated: 2017/11/07 16:06:44 by juhallyn         ###   ########.fr       */
+/*   Updated: 2017/11/07 16:51:53 by juhallyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,12 @@ void	right_direction(t_std **std, bool underline)
 		return ;
 	if ((*std)->select->next)
 	{
+		if ((*std)->select->next->status->place_index >= ((*std)->index_page + 1) * \
+		(*std)->max_elem_page)
+		{
+			reset(std, underline);
+			return ;
+		}
 		move_right(std, underline);
 		(*std)->select = (*std)->select->next;
 	}
